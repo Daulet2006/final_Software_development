ac67ae406f25543569c9dfd397be0fe0d652e5e5
Daulet2006 <dosbauyr@gmail.com>
2025-12-17 13:15:28 +0500

    helper enum for roles

--- src/main/java/awithd/finalproject/entity/User.java
+++ src/main/java/awithd/finalproject/entity/User.java
@@ -10,28 +10,38 @@ import java.util.List;
 
 @Entity
 @Table(name = "users")
-@AllArgsConstructor
-@NoArgsConstructor
-@Setter
+@Inheritance(strategy = InheritanceType.JOINED)
 @Getter
-@Builder
-public class User  implements UserDetails {
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User implements UserDetails {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String userName;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return List.of(() -> "ROLE_" + role.name());
     }
 
     @Override
     public String getUsername() {
-        return userName;
+        return email;
     }
 
-
+    @Override public boolean isAccountNonExpired() { return true; }
+    @Override public boolean isAccountNonLocked() { return true; }
+    @Override public boolean isCredentialsNonExpired() { return true; }
+    @Override public boolean isEnabled() { return true; }
 }
