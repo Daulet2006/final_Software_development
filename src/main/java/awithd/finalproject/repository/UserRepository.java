09669016a0a926121890f930d13ff698ce329ea7
teterna <altyndana.erbolkyzy@narxoz.kz>
2025-12-17 22:01:35 +0500

    create: fix jpa for user

--- src/main/java/awithd/finalproject/repository/UserRepository.java
+++ src/main/java/awithd/finalproject/repository/UserRepository.java
@@ -2,9 +2,9 @@ package awithd.finalproject.repository;
 
 import awithd.finalproject.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-    Optional<User> findByEmail(String email);
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
 }
