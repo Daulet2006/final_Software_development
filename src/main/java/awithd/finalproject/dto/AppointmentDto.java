61102fe05f1f887671cb6b3f63afcc61d5a28a47
Daulet2006 <dosbauyr@gmail.com>
2025-12-18 22:13:57 +0500

    dto: write artributs for appointment dti

--- src/main/java/awithd/finalproject/dto/AppointmentDto.java
+++ src/main/java/awithd/finalproject/dto/AppointmentDto.java
@@ -1,4 +1,25 @@
 package awithd.finalproject.dto;
 
+import awithd.finalproject.entity.AppointmentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class AppointmentDto {
+
+    private Long id;
+
+    private Long doctorId;
+    private Long patientId;
+
+    private LocalDateTime appointmentTime;
+    private AppointmentStatus status;
+    private String reason;
 }
