618c88acf0d20efd3d9dc456ec377790c1fa7666
Daulet2006 <dosbauyr@gmail.com>
2025-12-20 19:35:36 +0500

    mapper: did mapper for user and user new Annotation Mapppings it is omg

---
+++ src/main/java/awithd/finalproject/mapper/UserMapper.java
@@ -0,0 +1,29 @@
+package awithd.finalproject.mapper;
+
+import awithd.finalproject.dto.UserDto;
+import awithd.finalproject.entity.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    @Mappings({
+            @Mapping(target = "email", source = "emailDto"),
+            @Mapping(target = "password", source = "passwordDto"),
+            @Mapping(target = "firstName", source = "firstNameDto"),
+            @Mapping(target = "lastName", source = "lastNameDto"),
+            @Mapping(target = "permissions", source = "permissionsDto")
+    })
+    User toEntity(UserDto dto);
+
+    @Mappings({
+            @Mapping(target = "emailDto", source = "email"),
+            @Mapping(target = "passwordDto", ignore = true),
+            @Mapping(target = "firstNameDto", source = "firstName"),
+            @Mapping(target = "lastNameDto", source = "lastName"),
+            @Mapping(target = "permissionsDto", source = "permissions")
+    })
+    UserDto toDto(User user);
+}
