321988b3f5e50d9391382d2adb2a48fa520f647b
Daulet2006 <dosbauyr@gmail.com>
2025-12-25 15:45:21 +0500

    fix: add import

--- src/main/java/awithd/finalproject/controller/UserController.java
+++ src/main/java/awithd/finalproject/controller/UserController.java
@@ -5,10 +5,7 @@ import awithd.finalproject.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -25,9 +22,7 @@ public class UserController {
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestParam String email, @RequestParam String password) {
-
         userService.login(email, password);
-
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
