0bc9e6759d4baf4650d7aee2d54cbe9f043f2147
Daulet2006 <dosbauyr@gmail.com>
2025-12-26 09:44:50 +0500

    feat(me): add logic get <me> from basic auth

--- src/main/java/awithd/finalproject/service/impl/UserServiceImpl.java
+++ src/main/java/awithd/finalproject/service/impl/UserServiceImpl.java
@@ -9,6 +9,8 @@ import awithd.finalproject.repository.UserRepository;
 import awithd.finalproject.service.UserService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -36,7 +38,7 @@ public class UserServiceImpl implements UserService {
 
     @Override
     @Transactional
-    public User register(UserDto dto) {
+    public void register(UserDto dto) {
 
         if (userRepository.existsByEmail(dto.getEmailDto())) {
             throw new RuntimeException("User already exists");
@@ -58,6 +60,25 @@ public class UserServiceImpl implements UserService {
         user.setPermissions(permissions);
 
         userRepository.save(user);
-        return user;
     }
+
+    @Override
+    public UserDto getMe() {
+
+        Authentication authentication = SecurityContextHolder
+                .getContext()
+                .getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            throw new RuntimeException("Unauthorized");
+        }
+
+        String email = authentication.getName();
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return userMapper.toDto(user);
+    }
+
 }
